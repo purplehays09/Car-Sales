9cb995ea0eeb13cc3b650cd19fe3577418324b9f
david hays <purplehays527@gmail.com>
2020-09-08 13:33:42 -0600

    I raced ahead and got immediately stuck at the connection part

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+//1. Dependencies, 2. Store, 3. Provider,  4, Connect, 5. mapState to props
+
+const initialState = {}
+function reducer(){
+    return{
+        title:''
+    }
+}
+
+const store = createStore(reducer,initialState)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>
+
+, rootElement);
